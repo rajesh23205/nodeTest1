ad136589cf121b8443d55c3b078552dae0cf5dea
rajesh <rajesh@wisitech.com>
2020-04-14 20:21:25 +0530

    test body static file

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const api = require('./server/routes/api');
+// const api = require('./server/routes/api');
 
-app.get('/', (req, res) => res.send('Hello World'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 // app.use('/', api);
-// app.get("*",(req, res)=>{
-//     res.sendFile(path.join(__dirname,'dist/fagli/index.html'));
-// })
+app.get("/",(req, res)=>{
+    res.sendFile(path.join(__dirname,'dist/fagli/index.html'));
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
