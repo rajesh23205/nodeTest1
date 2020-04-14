e774247670f25137c4bce5ee4c7ebe1ca9f7d934
rajesh <rajesh@wisitech.com>
2020-04-14 19:31:12 +0530

    test body parser

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World! new'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
