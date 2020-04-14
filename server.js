82db1680ad3e213597577064b38e899c71638a85
rajesh <rajesh@wisitech.com>
2020-04-15 00:19:27 +0530

    angular scrach added

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const api = require('./server/routes/api');
 
+app.use(express.static(path.join(__dirname, 'fagli/dist/fagli')));
+
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -15,7 +17,7 @@ app.use('/', api);
 //     res.send("Hello world");
 // });
 app.get('*',(req, res)=>{
-    res.sendFile(path.join(__dirname,'dist/fagli/index.html'));
+    res.sendFile(path.join(__dirname,'fagli/dist/fagli/index.html'));
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
